5b46de0a4d1b5da7079b056f1e3116f9f2141de0
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-08-31 16:20:57 +0300

    Proje Ekranlarını Oluşturma

---
+++ android/app/src/main/kotlin/com/example/training41KahveniSecApp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.training41KahveniSecApp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:training41KahveniSecApp/screens/home/home.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Home();
+  }
+}
