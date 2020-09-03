98fa0d82ab7ce97dc7b7f0d09b168513ff5f0419
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Air.local>
2020-09-03 20:40:27 +0300

    Çekilen Verilere Göre Formu Düzenleme

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,3 +3,12 @@ class Kullanici {
 
   Kullanici({this.uid});
 }
+
+class KullaniciVeriler {
+  final String uid;
+  final String isim;
+  final String seker;
+  final int koyuluk;
+
+  KullaniciVeriler({this.uid, this.isim, this.seker, this.koyuluk});
+}
--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:training41KahveniSecApp/models/user.dart';
+import 'package:training41KahveniSecApp/services/database.dart';
 import 'package:training41KahveniSecApp/shared/contants.dart';
+import 'package:training41KahveniSecApp/shared/loading.dart';
 
 class SettingForm extends StatefulWidget {
   @override
@@ -16,59 +20,73 @@ class _SettingFormState extends State<SettingForm> {
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: _formKey,
-      child: Column(
-        children: [
-          Text(
-            "Ayarları Güncelleyebilirsiniz.",
-            style: TextStyle(fontSize: 18),
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          TextFormField(
-            decoration: inputDecoration.copyWith(
-                hintText: "İsim", icon: Icon(Icons.person)),
-            validator: (val) => val.isEmpty ? "Lütfen isim giriniz" : null,
-            onChanged: (val) => setState(() => currentIsim = val),
-          ),
-          SizedBox(
-            height: 10.0,
-          ),
-          DropdownButtonFormField(
-            items: sekerler.map((seker) {
-              return DropdownMenuItem(
-                value: seker,
-                child: Text("$seker şeker"),
-              );
-            }).toList(),
-            onChanged: (val) => setState(() => currentSeker = val),
-            decoration: inputDecoration.copyWith(
-                icon: Icon(Icons.format_color_fill,
-                    color: Color.fromRGBO(90, 46, 46, 1))),
-            value: currentSeker ?? "0",
-          ),
-          Slider(
-            value: (currentKoyuluk ?? 100).toDouble(),
-            min: 100,
-            max: 900,
-            divisions: 8,
-            onChanged: (val) => setState(() => currentKoyuluk = val.round()),
-            activeColor: Colors.brown[currentKoyuluk ?? 100],
-            inactiveColor: Colors.brown[currentKoyuluk ?? 100],
-          ),
-          RaisedButton(
-              color: Color.fromRGBO(90, 46, 46, 1),
-              child: Text("Güncelle",
-                  style: TextStyle(color: Color.fromRGBO(255, 254, 223, 1))),
-              onPressed: () async {
-                print(currentIsim);
-                print(currentSeker);
-                print(currentKoyuluk);
-              })
-        ],
-      ),
-    );
+    final user = Provider.of<Kullanici>(context);
+    return StreamBuilder<KullaniciVeriler>(
+        stream: DatabaseService(uid: user.uid).kullaniciVerileri,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            KullaniciVeriler kv = snapshot.data;
+            return Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  Text(
+                    "Ayarları Güncelleyebilirsiniz.",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  TextFormField(
+                    initialValue: kv.isim,
+                    decoration: inputDecoration.copyWith(
+                        hintText: "İsim", icon: Icon(Icons.person)),
+                    validator: (val) =>
+                        val.isEmpty ? "Lütfen isim giriniz" : null,
+                    onChanged: (val) => setState(() => currentIsim = val),
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  DropdownButtonFormField(
+                    items: sekerler.map((seker) {
+                      return DropdownMenuItem(
+                        value: seker,
+                        child: Text("$seker şeker"),
+                      );
+                    }).toList(),
+                    onChanged: (val) => setState(() => currentSeker = val),
+                    decoration: inputDecoration.copyWith(
+                        icon: Icon(Icons.format_color_fill,
+                            color: Color.fromRGBO(90, 46, 46, 1))),
+                    value: currentSeker ?? kv.seker,
+                  ),
+                  Slider(
+                    value: (currentKoyuluk ?? kv.koyuluk).toDouble(),
+                    min: 100,
+                    max: 900,
+                    divisions: 8,
+                    onChanged: (val) =>
+                        setState(() => currentKoyuluk = val.round()),
+                    activeColor: Colors.brown[currentKoyuluk ?? kv.koyuluk],
+                    inactiveColor: Colors.brown[currentKoyuluk ?? kv.koyuluk],
+                  ),
+                  RaisedButton(
+                      color: Color.fromRGBO(90, 46, 46, 1),
+                      child: Text("Güncelle",
+                          style: TextStyle(
+                              color: Color.fromRGBO(255, 254, 223, 1))),
+                      onPressed: () async {
+                        print(currentIsim);
+                        print(currentSeker);
+                        print(currentKoyuluk);
+                      })
+                ],
+              ),
+            );
+          } else {
+            return Loading();
+          }
+        });
   }
 }
